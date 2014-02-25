325fd081fa3f6fa8a65098ffbcbdc4919299b967
simonboeuf <simon.boeuf@gmail.com>
2014-02-25 21:24:05 +0100

    dirty fix on quantified masks

--- check_char.c
+++ check_char.c
@@ -2,26 +2,99 @@
 
 static int	check_star(char **format, char **string, int mask_pos)
 {
-	while (**format == **string)
-		(*string)++;
+	int	cont;
+	int	i;
+
+	cont = 1;
+	if (mask_pos == 0)
+	{
+		while (**format == **string)
+			*string += 1;
+	}
+	else
+	{
+		cont = 1;
+		while (cont)
+		{
+			i = 0;
+			cont = 0;
+			while (i < mask_pos)
+			{
+				if (*((*format) + i) == **string)
+					cont = 1;
+				i++;
+			}
+			if (cont)
+				*string += 1;
+		}
+	}
 	(*format) += mask_pos + 2;
 	return (1);
 }
 
 static int	check_inter(char **format, char **string, int mask_pos)
 {
-	if (**format == **string)
-		*string += 1;
+	int	i;
+
+	if (mask_pos == 0)
+	{
+		if (**format == **string)
+			*string += 1;
+	}
+	else
+	{
+		i = 0;
+		while (i < mask_pos)
+		{
+			if (*((*format) + i) == **string)
+				*string += 1;
+			i++;
+		}
+	}
 	(*format) += mask_pos + 2;
 	return (1);
 }
 
 static int	check_plus(char **format, char **string, int mask_pos)
 {
-	if (**format != **string)
-		return (0);
-	while (**format == **string)
-		*string += 1;
+	int	cont;
+	int	i;
+
+	cont = 1;
+	if (mask_pos == 0)
+	{
+		if (**format != **string)
+			return (0);
+		while (**format == **string)
+			*string += 1;
+	}
+	else
+	{
+		cont = 0;
+		i = 0;
+		while (i < mask_pos)
+		{
+			if (*((*format) + i) == **string)
+				cont = 1;
+			i++;
+		}
+		if (!cont)
+			return (0);
+		cont = 1;
+		while (cont)
+		{
+			i = 0;
+			cont = 0;
+			while (i < mask_pos)
+			{
+				if (*((*format) + i) == **string)
+					cont = 1;
+				i++;
+			}
+			if (cont)
+				*string += 1;
+		}
+	}
 	(*format) += mask_pos + 2;
 	return (1);
 }
